d0b3f555b3b26b6c66b0daba1be88dd7ed637920
yonglanliu <122495576+yonglanliu@users.noreply.github.com>
2026-01-27 06:33:00 +0000

    Deploying to gh-pages from @ yonglanliu/yonglanliu.github.io@08b42552532012e0c5179ac54586ef2cab37414c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -157,42 +157,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Yonglan-Liu_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%6C%69%75%79%6F%6E%67%6C%61%6E%30%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=eRfVtdwAAAAJ&hl=en&oi=ao", "_blank");
         },
       },{
       id: 'light-theme',
